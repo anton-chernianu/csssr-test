23b0edbf4b4c26b4b15c273d4e5d705a41b457da
Anton <anton@chernianu.com>
2017-12-25 17:40:57 +0200

    skill range

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,16 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-$(() => {
-	svg4everybody();
+// NoUISlider Setting
+var directionSlider = document.querySelector('.skill-range');
+
+noUiSlider.create(directionSlider, {
+	start: 30,
+	range: {
+		'min': 0,
+		'max': 100
+	}
 });
 
-// // NoUISlider Setting
-// let directionSlider = document.querySelector('.skill-range');
-
-// noUiSlider.create(directionSlider, {
-// 	start: 30,
-// 	range: {
-// 		'min': 0,
-// 		'max': 100
-// 	}
+// import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
+// $(() => {
+// 	svg4everybody();
 // });
